633555b16279c0f907653eee1c6d9aab648cf00a
emiliocheval <emil.conradsson@hotmail.com>
2024-01-24 19:53:12 +0100

    Added components

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+    </div>
   )
 }
 
