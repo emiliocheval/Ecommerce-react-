304c1be59ccb4e81e4c38d0f38ea2643ecb4d6b3
emiliocheval <emil.conradsson@hotmail.com>
2024-01-27 21:52:57 +0100

    Additional components

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,30 @@ import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
-
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login-signup" element={<LoginSignup />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/:category" element={<ShopCategory />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category='men' />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category='women' />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category='kid' />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
